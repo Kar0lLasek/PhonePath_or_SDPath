a7141c2556a5441f436b4bc62f725d1e966ad70a
Kar0lLasek <19karolasek98@gmail.com>
2020-03-23 12:58:48 +0100

    Add project files.

---
+++ MainActivity.cs
@@ -0,0 +1,123 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android;
+using Plugin.Media;
+using Android.Graphics;
+using System.IO;
+using Android.Content;
+
+namespace simple_Camera
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        Button btnDoPhoto, btnSettings, check;
+        ImageView ImageViewLastPhoto;
+        TextView txtPath;
+
+        readonly string[] permissionGroup =
+        {
+            Manifest.Permission.ReadExternalStorage,
+            Manifest.Permission.WriteExternalStorage,
+            Manifest.Permission.Camera
+        };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btnDoPhoto = (Button)FindViewById(Resource.Id.btnDoPhoto);
+            btnSettings = (Button)FindViewById(Resource.Id.btnSettings);
+            ImageViewLastPhoto = (ImageView)FindViewById(Resource.Id.ImageViewLastPhoto);
+            txtPath = (TextView)FindViewById(Resource.Id.txtPath);
+            check = (Button)FindViewById(Resource.Id.check);
+
+            check.Click += checkFunction;
+            btnDoPhoto.Click += DoPhoto_Click;
+            btnSettings.Click += Settings_Click;
+            RequestPermissions(permissionGroup, 0);
+        }
+
+        private void checkFunction(object sender, EventArgs e)
+        {
+            /*Toast.MakeText(this, "SD card path: " + GetBaseFolderPath(true), ToastLength.Long).Show();
+            Toast.MakeText(this, "Phone path: " + GetBaseFolderPath(false), ToastLength.Long).Show();*/
+            if(Directory.Exists(MainActivity.GetBaseFolderPath(true) + "/CheckThisOutOnSD"))
+            {
+                Toast.MakeText(this, "WORKED", ToastLength.Long).Show();
+            }
+        }
+
+        public static string GetBaseFolderPath(bool getSDPath = false)
+        {
+            string baseFolderPath = "";
+
+            try
+            {
+                Context context = Application.Context;
+                Java.IO.File[] dirs = context.GetExternalFilesDirs(null);
+
+                foreach (Java.IO.File folder in dirs)
+                {
+                    bool IsRemovable = Android.OS.Environment.InvokeIsExternalStorageRemovable(folder);
+                    bool IsEmulated = Android.OS.Environment.InvokeIsExternalStorageEmulated(folder);
+
+                    if (getSDPath ? IsRemovable && !IsEmulated : !IsRemovable && IsEmulated)
+                        baseFolderPath = folder.Path;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("GetBaseFolderPath caused the follwing exception: {0}", ex);
+            }
+
+            return baseFolderPath;
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+            StartActivity(new Android.Content.Intent(this, typeof(SettingsActivity)));
+        }
+
+        private void DoPhoto_Click(object sender, EventArgs e)
+        {
+            TakePhoto();
+        }
+
+        async void TakePhoto()
+        {
+            await CrossMedia.Current.Initialize();
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+                CompressionQuality = 40,
+                Name = "myimage.jpg",
+                Directory = "newDirectory",
+                SaveToAlbum = true
+            });
+
+            if (file == null)
+                return;
+
+            //Check the last taken photo
+            byte[] imageArray = System.IO.File.ReadAllBytes(file.Path);
+            //Toast.MakeText(this, file.ToString(), ToastLength.Long).Show();
+            Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
+            ImageViewLastPhoto.SetImageBitmap(bitmap);
+            txtPath.Text = file.Path;
+            
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ SettingsActivity.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using Java.Nio.Channels;
+
+namespace simple_Camera
+{
+    [Activity(Label = "SettingsActivity")]
+    public class SettingsActivity : Activity
+    {
+        TextView tvShowPaths;
+        Button copy;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.activity_settings);
+
+            tvShowPaths = (TextView)FindViewById(Resource.Id.tvShowPaths);
+            tvShowPaths.Text = "Witam w Settingsach";
+            copy = (Button)FindViewById(Resource.Id.copy);
+
+            copy.Click += CopyFunction;
+
+        }
+
+        private void CopyFunction(object sender, EventArgs e)
+        {
+            if(Directory.Exists(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures).ToString() + "/newDirectory"))
+            {
+                Toast.MakeText(this, "Działa wiec kopiujemy do nowej", ToastLength.Long).Show();
+                try
+                {
+                    string sourceDir = Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures).ToString() + "/newDirectory";
+                    string destDir = MainActivity.GetBaseFolderPath(true) + "/CheckThisOutOnSD";
+                    string[] picList = Directory.GetFiles(sourceDir, "*.jpg");
+                    
+                    if(!Directory.Exists(destDir))
+                        Directory.CreateDirectory(destDir);
+
+                    foreach (string f in picList)
+                    {
+                        Toast.MakeText(this, f, ToastLength.Long).Show();
+                        string fName = f.Substring(sourceDir.Length + 1);
+                        System.IO.File.Copy(Path.Combine(sourceDir, fName), Path.Combine(destDir, fName), true);
+                    }
+
+                    foreach (string f in picList)
+                    {
+                        System.IO.File.Delete(f);
+                    }
+
+                } catch (DirectoryNotFoundException dirNotFound) {
+                    Toast.MakeText(this, dirNotFound.ToString(), ToastLength.Long).Show();
+                }
+                //Copy(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures).ToString() + "/newDirectory", Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures).ToString() + "/CheckThis");
+                Toast.MakeText(this, "DONE", ToastLength.Long).Show();
+            }
+        }
+
+        void Copy(string sourceDir, string targetDir)
+        {
+            if(!Directory.Exists(targetDir))
+                Directory.CreateDirectory(targetDir);
+
+            foreach (var file in Directory.GetFiles(sourceDir))
+            {
+                System.IO.File.Move(file, Path.Combine(targetDir, Path.GetFileName(file)));
+                Toast.MakeText(this, file, ToastLength.Long).Show();
+            }
+                
+
+            foreach (var directory in Directory.GetDirectories(sourceDir))
+                Copy(directory, Path.Combine(targetDir, Path.GetFileName(directory)));
+        }
+    }
+}
